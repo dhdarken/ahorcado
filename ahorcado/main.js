8b11e19055bf3b46783bbaa5dc7f75fc9c3d01f6
julian <juliansanvill@hotmail.com>
2023-03-17 12:09:24 -0500

    plan work and markup hangman SVG

--- ahorcado/main.js
+++ ahorcado/main.js
@@ -1,12 +1,20 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-   julio
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+/**
+ * PASOS 
+ * 
+ * 1. maquetar el html para que se muestre la figura de ahorcado (y esconder el cuerpo) done
+ * 2. generar una lista de posibles palabras en un arreglo de JS
+ * 3. hacer una función que escoja una palabra de la lista al azar
+ * 4. maquetar el html para mostrar el numero de espacios que tiene la palabra
+ * 5. generar el html para la entrada de la letra que se va a adivinar (1 sola letra )
+ * 6. buscar en la palabra las ocurrencias para la letra seleccionada
+ * 7. si la letra existe llenar los espacios que contienen la letra ingresada
+ * 8. sino existe la letra mostrar la siguiente parte del ahorcado y agregar la letra a la lista de letras usadas
+ * 9. definir el numero de intentos
+ * 10. ganas si descubres todos los espacios
+ * 11. pierdes si se te agotan los intentos
+ * 
+ */
